1fe5d92aa8361412b4025f562f32ea9fa49dab85
Dawid Garncarz <dawid.garncarz@mytracko.pl>
2025-12-15 18:56:52 +0100

    Added endpoints for updating, and fetching categories

--- backend/src/main/java/com/haalier/moneymanager/controller/CategoryController.java
+++ backend/src/main/java/com/haalier/moneymanager/controller/CategoryController.java
@@ -3,12 +3,12 @@ package com.haalier.moneymanager.controller;
 import com.haalier.moneymanager.dto.CategoryDTO;
 import com.haalier.moneymanager.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +22,24 @@ public class CategoryController {
         CategoryDTO savedCategory = categoryService.createCategory(categoryDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getCategories() {
+        List<CategoryDTO> categories = categoryService.getCategoriesForCurrentUser();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<List<CategoryDTO>> getCategoriesByTypeForCurrentUser(@PathVariable String type) {
+        List<CategoryDTO> categories = categoryService.getCategoriesByTypeForCurrentUser(type);
+        return ResponseEntity.ok(categories);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long categoryId, @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO updatedCategory = categoryService.updateCategory(categoryId, categoryDTO);
+        return ResponseEntity.ok(updatedCategory);
     }
 
 }
--- backend/src/main/java/com/haalier/moneymanager/service/CategoryService.java
+++ backend/src/main/java/com/haalier/moneymanager/service/CategoryService.java
@@ -7,7 +7,8 @@ import com.haalier.moneymanager.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,14 +22,38 @@ public class CategoryService {
         ProfileEntity profile = profileService.getCurrentProfile();
 
         if (categoryRepository.existsByNameAndProfileId(categoryDTO.getName(), profile.getId())) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Category already exists.");
-        };
+            throw new RuntimeException("Category already exists.");
+        }
+        ;
 
         CategoryEntity newCategory = toEntity(categoryDTO, profile);
         newCategory = categoryRepository.save(newCategory);
         return toDTO(newCategory);
     }
 
+    public List<CategoryDTO> getCategoriesForCurrentUser() {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        List<CategoryEntity> categories = categoryRepository.findByProfileId(profile.getId());
+        return categories.stream().map(this::toDTO).toList();
+    }
+
+
+    public List<CategoryDTO> getCategoriesByTypeForCurrentUser(String type) {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        List<CategoryEntity> categories = categoryRepository.findByTypeAndProfileId(type, profile.getId());
+        return categories.stream().map(this::toDTO).toList();
+    }
+
+    public CategoryDTO updateCategory(Long categoryId, CategoryDTO categoryDTO) {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        CategoryEntity categoryToUpdate =
+                categoryRepository.findByIdAndProfileId(categoryId, profile.getId())
+                        .orElseThrow(() -> new RuntimeException("Category not found or not accessible."));
+        categoryToUpdate.setName(categoryDTO.getName());
+        categoryToUpdate.setIcon(categoryDTO.getIcon());
+        categoryToUpdate = categoryRepository.save(categoryToUpdate);
+        return toDTO(categoryToUpdate);
+    }
 
     // helper methods
     private CategoryEntity toEntity(CategoryDTO categoryDTO, ProfileEntity profile) {
