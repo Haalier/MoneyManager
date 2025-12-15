6fcd0aef6d461b2ce3bbb205a74ab11b8d86d808
Dawid Garncarz <dawid.garncarz@mytracko.pl>
2025-12-15 17:28:58 +0100

    Created controller, dto and service for category table

--- backend/src/main/java/com/haalier/moneymanager/service/CategoryService.java
+++ backend/src/main/java/com/haalier/moneymanager/service/CategoryService.java
@@ -1,4 +1,54 @@
 package com.haalier.moneymanager.service;
 
+import com.haalier.moneymanager.dto.CategoryDTO;
+import com.haalier.moneymanager.entity.CategoryEntity;
+import com.haalier.moneymanager.entity.ProfileEntity;
+import com.haalier.moneymanager.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final ProfileService profileService;
+    private final CategoryRepository categoryRepository;
+
+
+    public CategoryDTO createCategory(CategoryDTO categoryDTO) {
+        ProfileEntity profile = profileService.getCurrentProfile();
+
+        if (categoryRepository.existsByNameAndProfileId(categoryDTO.getName(), profile.getId())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Category already exists.");
+        };
+
+        CategoryEntity newCategory = toEntity(categoryDTO, profile);
+        newCategory = categoryRepository.save(newCategory);
+        return toDTO(newCategory);
+    }
+
+
+    // helper methods
+    private CategoryEntity toEntity(CategoryDTO categoryDTO, ProfileEntity profile) {
+        return CategoryEntity.builder()
+                .name(categoryDTO.getName())
+                .type(categoryDTO.getType())
+                .icon(categoryDTO.getIcon())
+                .profile(profile)
+                .build();
+    }
+
+    private CategoryDTO toDTO(CategoryEntity category) {
+        return CategoryDTO.builder()
+                .id(category.getId())
+                .profileId(category.getProfile() != null ? category.getProfile().getId() : null)
+                .name(category.getName())
+                .icon(category.getIcon())
+                .type(category.getType())
+                .createdAt(category.getCreatedAt())
+                .updatedAt(category.getUpdatedAt())
+                .build();
+    }
 }
